60511bd282c6836ca18ecf9e3df14209f443a519
BakayAlexander <bakay.dvr@gmail.com>
2022-05-26 19:50:30 +0300

    refactor: big changes at cod style

--- src/components/TasksList/TasksList.js
+++ src/components/TasksList/TasksList.js
@@ -1,27 +1,39 @@
 import React, { useState } from 'react';
-import './TasksList.css';
-import Task from '../Task/Task';
-import Popup from '../Popup/Popup';
 import { useDispatch, useSelector } from 'react-redux';
 import { createTaskAction, updateTaskAction } from '../../store/actions';
 import { isLoadingSelector, tasksSelector, tokenSelector } from '../../store/selectors';
 import SortPanel from '../SortPanel/SortPanel';
 import Preloader from '../Preloader/Preloader';
+import './TasksList.css';
+import Task from '../Task/Task';
+import Popup from '../Popup/Popup';
+import { selectOptions } from '../../utils/config';
+import FormUpdate from '../FormUpdate/FormUpdate';
+import FormAdd from '../FormAdd/FormAdd';
 
 function TasksList() {
+	const dispatch = useDispatch();
 	const [isAddPopupOpen, setIsAddPopupOpen] = useState(false);
 	const [isUpdatePopupOpen, setIsUpdatePopupOpen] = useState(false);
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-	const [task, setTask] = useState('');
-	const [taskUpdate, setTaskUpdate] = useState('');
-	const [statusUpdate, setStatusUpdate] = useState('');
-	const [idUpdate, setIdUpdate] = useState('');
-
 	const tasks = useSelector(tasksSelector);
 	const token = useSelector(tokenSelector);
 	const isLoading = useSelector(isLoadingSelector);
-	const dispatch = useDispatch();
+
+	const initialState = {
+		name: '',
+		email: '',
+		task: '',
+		taskUpdate: '',
+		statusUpdate: '',
+		idUpdate: '',
+	};
+
+	const [taskValues, setTaskValues] = useState(initialState);
+
+	const changeValue = (e) => {
+		const { name, value } = e.target;
+		setTaskValues({ ...taskValues, [name]: value });
+	};
 
 	function handleClickAddButton() {
 		setIsAddPopupOpen(true);
@@ -29,9 +41,7 @@ function TasksList() {
 
 	function handleClickUpdateButton(id, status, task) {
 		setIsUpdatePopupOpen(true);
-		setIdUpdate(id);
-		setStatusUpdate(status);
-		setTaskUpdate(task);
+		setTaskValues({ ...taskValues, idUpdate: id, statusUpdate: status, taskUpdate: task });
 	}
 
 	function onClosePopup() {
@@ -39,38 +49,16 @@ function TasksList() {
 		setIsUpdatePopupOpen(false);
 	}
 
-	function handleChangeName(e) {
-		setName(e.target.value);
-	}
-
-	function handleChangeEmail(e) {
-		setEmail(e.target.value);
-	}
-
-	function handleChangeTask(e) {
-		setTask(e.target.value);
-	}
-
-	function handleChangeTaskUpdate(e) {
-		setTaskUpdate(e.target.value);
-	}
-
-	function handleChangeStatusUpdate(e) {
-		setStatusUpdate(e.target.value);
-	}
-
 	function handleSubmitCreateTask(e) {
 		e.preventDefault();
-		dispatch(createTaskAction(name, email, task));
+		dispatch(createTaskAction(taskValues.name, taskValues.email, taskValues.task));
 		onClosePopup();
-		setName('');
-		setEmail('');
-		setTask('');
+		setTaskValues({ ...taskValues, name: '', email: '', task: '' });
 	}
 
 	function handleSubmitUpdateTask(e) {
 		e.preventDefault();
-		dispatch(updateTaskAction(taskUpdate, Number(statusUpdate), token, idUpdate));
+		dispatch(updateTaskAction(taskValues.taskUpdate, Number(taskValues.statusUpdate), token, taskValues.idUpdate));
 		onClosePopup();
 	}
 
@@ -90,79 +78,22 @@ function TasksList() {
 				</ul>
 			)}
 			<Popup isOpen={isAddPopupOpen} onClose={onClosePopup}>
-				<form className='task-list' onSubmit={handleSubmitCreateTask} title='Add new station'>
-					<h2 className='task-list__title'>Add new task</h2>
-					<label className='task-list__label'>
-						Username:
-						<input
-							className='task-list__input'
-							id='name-input'
-							type='text'
-							autoComplete='none'
-							placeholder='Please enter name'
-							required
-							value={name ?? ''}
-							onChange={handleChangeName}
-						></input>
-					</label>
-					<label className='task-list__label'>
-						Email:
-						<input
-							className='task-list__input'
-							id='comment-input'
-							type='email'
-							pattern="(?!(^[.-].*|[^@]*[.-]@|.*\.{2,}.*)|^.{254}.)([a-zA-Z0-9!#$%&'*+\/=?^_`{|}~.-]+@)(?!-.*|.*-\.)([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,15}"
-							autoComplete='none'
-							placeholder='Please enter email'
-							required
-							value={email ?? ''}
-							onChange={handleChangeEmail}
-						></input>
-					</label>
-					<label className='task-list__label'>
-						Task:
-						<input
-							className='task-list__input'
-							id='comment-input'
-							type='text'
-							autoComplete='none'
-							placeholder='Please enter task'
-							required
-							value={task ?? ''}
-							onChange={handleChangeTask}
-						></input>
-					</label>
-					<button className='task-list__submit-button' type='submit'>
-						Add new task
-					</button>
-				</form>
+				<FormAdd
+					onSubmit={handleSubmitCreateTask}
+					nameValue={taskValues.name}
+					emailValue={taskValues.email}
+					taskValue={taskValues.task}
+					onChangeValue={changeValue}
+				/>
 			</Popup>
 			<Popup isOpen={isUpdatePopupOpen} onClose={onClosePopup}>
-				<form className='task-list' onSubmit={handleSubmitUpdateTask} title='Add new station'>
-					<h2 className='task-list__title'>Update task</h2>
-					<label className='task-list__label'>
-						Status:
-						<select className='task-list__select' value={statusUpdate} onChange={handleChangeStatusUpdate}>
-							<option value={0}>Not completed. Not edited.</option>
-							<option value={1}>Not completed. Edited.</option>
-							<option value={10}>Completed. Not edited.</option>
-							<option value={11}>Completed. Edited.</option>
-						</select>
-					</label>
-					<label className='task-list__label'>
-						Task:
-						<textarea
-							className='task-list__textarea'
-							type='text'
-							placeholder='Please enter task'
-							value={taskUpdate ?? ''}
-							onChange={handleChangeTaskUpdate}
-						></textarea>
-					</label>
-					<button className='task-list__submit-button' type='submit'>
-						Update
-					</button>
-				</form>
+				<FormUpdate
+					onSubmit={handleSubmitUpdateTask}
+					onChangeValue={changeValue}
+					statusUpdateValue={taskValues.statusUpdate}
+					taskUpdateValue={taskValues.taskUpdate}
+					selectOptions={selectOptions}
+				/>
 			</Popup>
 		</>
 	);
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,9 @@
 export const BASE_URL = 'https://uxcandy.com/~shapoval/test-task-backend/v2/';
 export const BASE_DEVELOPER = '?developer=BakayAlexander';
+
+export const selectOptions = [
+	{ value: 0, label: 'Not completed. Not edited.' },
+	{ value: 1, label: 'Not completed. Edited.' },
+	{ value: 10, label: 'Completed. Not edited.' },
+	{ value: 11, label: 'Completed. Edited.' },
+];
